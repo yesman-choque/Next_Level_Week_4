758088bddd9fc09566e67f7124110d20f58dda6c
Yesman Choque <yesman.choque@gmail.com>
2021-02-25 20:15:25 -0300

    Context and Components

---
+++ moveit-next/src/components/ChallengedBox.tsx
@@ -0,0 +1,48 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/ChallengedBox.module.css';
+
+export function ChallegedBox() {
+    const { activeChallenge, resetChallenge } = useContext(ChallengesContext)
+
+    return (
+        <div className={styles.challengedBoxContainer}>
+            { activeChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe {activeChallenge.amount} xp</header>
+
+                    <main>
+                        <img src={`icons/${activeChallenge.type}.svg`} />
+                        <strong>Novo desafio</strong>
+                        <p>{activeChallenge.description}</p>  
+                    </main>
+
+                    <footer>
+                        <button 
+                            type="button"
+                            className={styles.challengeFailedButton}
+                            onClick={resetChallenge}
+                        >
+                            Falhei
+                        </button>
+                        <button 
+                            type="button"
+                            className={styles.challengeSucceededButton}
+                        >
+                            Completei
+                        </button>
+                    </footer>
+                </div>
+            ) : (
+                <div className={styles.challengedNotActive}>
+                    <strong>Finalize um ciclo para receber um desafio</strong>
+                    <p>
+                        <img src="icons/level-up.svg" alt="Level Up" />
+                        Avance de level completando desafios.
+                    </p>
+                </div>
+            ) }
+
+        </div>
+    )
+}
